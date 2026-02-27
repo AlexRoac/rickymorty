dee4f9cc88d75a3fb194f6a255bb4073d7b96e6b
AlexRoac <alejandro.roac05@gmail.com>
2026-02-27 17:06:12 -0600

    correcion

--- src/data/index.ts
+++
@@ -1,3 +0,0 @@
-// Aquí centralizamos la exportación de nuestros datos (mock data)
-// De esta forma es más fácil importar los datos desde otros archivos
-export {default as POKEMON_MOCK} from "./pokemon.data"
